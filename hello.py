80a6cc2e9360a06276269d548b9e3e04d3ce57d0
niatalwalkar <105653169+niatalwalkar@users.noreply.github.com>
2025-09-20 13:57:24 -0400

    Activity 1.2 - Example 2-2

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
+#example 2-1
+# from flask import Flask 
+# app = Flask(__name__) 
+# @app.route('/') 
+# def index(): 
+#     return '<h1>Hello World!</h1>'
 
+#example 2-2
 from flask import Flask 
 app = Flask(__name__) 
 @app.route('/') 
 def index(): 
-    return '<h1>Hello World!<h1>'
+    return '<h1>Hello World!</h1>'
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
