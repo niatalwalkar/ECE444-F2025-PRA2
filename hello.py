69ca615bc38151d01c4db5a837386210552350da
niatalwalkar <105653169+niatalwalkar@users.noreply.github.com>
2025-09-21 14:19:37 -0400

    Activity 1.3: Adding navigation tabs & timestamp

--- hello.py
+++ hello.py
@@ -6,12 +6,34 @@
 #     return '<h1>Hello World!</h1>'
 
 #example 2-2
-from flask import Flask 
-app = Flask(__name__) 
-@app.route('/') 
-def index(): 
-    return '<h1>Hello World!</h1>'
+# from flask import Flask
+# app = Flask(__name__) 
+# @app.route('/') 
+# def index(): 
+#     return '<h1>Hello World!</h1>'
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello, {}!</h1>'.format(name)
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+app = Flask(__name__) 
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/user/<name>')
+# def user(name):
+#     return render_template('user.html', name = name)
+
+@app.route('/')
+def base():
+    return render_template("base.html", name = "Nia", current_time = datetime.utcnow())
+ 
